fc3bc6057179b5ff438613d0c9620e6fc2193c63
sami0055 <samiul.alim01@northsouth.edu>
2021-10-11 15:10:34 +0600

    TASK

---
+++ class01/src/class01/Task1.java
@@ -0,0 +1,12 @@
+package class01;
+
+public class Task1 {
+ 
+	//psvm
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
